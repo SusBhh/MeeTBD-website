7ac3f1405f5a90c85a5f844c1da031cf9edeafac
Isabel Mitre <isamitre@gmail.com>
2023-11-15 20:19:12 -0500

    start code for join group, doesn't work yet

--- meetbd/src/pages/GroupsPage.js
+++ meetbd/src/pages/GroupsPage.js
@@ -10,29 +10,70 @@ const GroupsPage = () => {
   const [groupName, setGroupName] = React.useState("");
   const [joinID, setJoinID] = React.useState("");
 
-  // TODO: join by ID
-  // const updateItem = async ({ item, id }) => {
-  //     setLoading(true);
-  //     try {
-  //       const user = supabase.auth.user();
-
-  //       const { error } = await supabase
-  //         .from("todo")
-  //         .update({ item })
-  //         .eq("userId", user?.id)
-  //         .eq("id", id); //matching id of row to update
-
-  //       if (error) throw error;
-
-  //       await getActiveItems();
-  //     } catch (error) {
-  //       alert(error.error_description || error.message);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
   const supabase = useSupabaseClient();
+
+  async function handleJoinGroup(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+
+    if (formJson["joinID"] === "") {
+      // return early if groupName is empty
+      return;
+    }
+
+    // break up join ID (groupID + groupName, no spaces)
+    const currGroupID = formJson["joinID"].match(/\d+/g);
+    const currGroupNameSquish = formJson["joinID"].substring(
+      currGroupID.length
+    );
+
+    // get user
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    // read curr group's members + name
+    const { readError, data } = await supabase
+      .from("groups")
+      .select("name, members")
+      .eq("id", currGroupID);
+    if (readError) throw readError;
+
+    // get curr group's members + name
+    let members = [];
+    let currGroupName = "";
+    console.log(data);
+    if (data) {
+      members = data["members"];
+      currGroupName = data["name"];
+    }
+
+    // check that the group names match
+    if (
+      currGroupNameSquish.toString().replace(/\s/g, "") !=
+      currGroupName.toString().replace(/\s/g, "")
+    ) {
+      alert("There is no group with that join ID.");
+      return;
+    }
+
+    // append user id to members
+    members.push(user?.id);
+
+    // update curr group with additional member
+    const { updateError } = await supabase
+      .from("groups")
+      .update({ members: members })
+      .eq("id", currGroupID);
+    if (updateError) throw updateError;
+
+    alert("Successfully joined group " + currGroupName + "!");
+
+    setJoinID("");
+  }
 
   async function handleCreateGroup(e) {
     e.preventDefault();
@@ -45,6 +86,8 @@ const GroupsPage = () => {
       // return early if groupName is empty
       return;
     }
+    // TODO: more group name validation
+    // TODO: prevent number from starting groupname
 
     // get user
     const {
@@ -67,14 +110,14 @@ const GroupsPage = () => {
     <div>
       <h1>Groups Page</h1>
       <div>
-        {/* list groups here */}
+        {/* TODO: list groups here */}
         <ReadGroups />
       </div>
       <div className="bottom">
         <div className="add-group">
           {/* add groups here */}
           <h2>Join an Existing Group by ID</h2>
-          <form>
+          <form onSubmit={handleJoinGroup}>
             <label>
               <input
                 type="text"
