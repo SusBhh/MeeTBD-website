4646026f4c08074bf1ef10995619311e92580361
Isabel Mitre <isamitre@gmail.com>
2023-11-21 21:38:27 -0600

    read works!

--- meetbd/src/components/ReadGroups.js
+++ meetbd/src/components/ReadGroups.js
@@ -1,27 +1,19 @@
 import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 import Group from "./Group";
 
 const ReadGroups = () => {
-  const [groups, setGroups] = React.useState([
-    {
-      id: 10,
-      name: "hello3",
-      owner: "1a415398-aff6-4b23-9587-8067f10796bc",
-      members: ["1a415398-aff6-4b23-9587-8067f10796bc"],
-    },
-    {
-      id: 12,
-      name: "hello2",
-      owner: "1a415398-aff6-4b23-9587-8067f10796bc",
-      members: ["1a415398-aff6-4b23-9587-8067f10796bc"],
-    },
-  ]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [groups, setGroups] = React.useState([]);
 
   const supabase = useSupabaseClient();
 
   async function readGroups() {
+    // set isLoading to true
+    setIsLoading(true);
+
     // get user
     const {
       data: { user },
@@ -31,11 +23,13 @@ const ReadGroups = () => {
     const { error, data } = await supabase
       .from("groups") //the table you want to work with
       .select("id, name, owner, members") // columns to select from the database
-      .in("members", user?.id) // match only groups where user is a member
+      .contains("members", [user?.id]) // match only groups where user is a member
       .order("id", { ascending: false }); // sort the data so the last item comes on top;
+
     if (error) throw error; // check if there was an error fetching the data and move the execution to the catch block
     if (data) setGroups(data);
-    console.log(groups);
+
+    setIsLoading(false);
   }
 
   React.useEffect(() => {
@@ -44,12 +38,14 @@ const ReadGroups = () => {
 
   return (
     <div>
-      {groups.length == 0 ? (
+      {isLoading ? (
+        <CircularProgress />
+      ) : groups.length == 0 ? (
         <p>No groups to display</p>
       ) : (
         <div>
           {groups.map((group, i) => (
-            <Group group={group} />
+            <Group group={group} key={i} />
           ))}
         </div>
       )}
