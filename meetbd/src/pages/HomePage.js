6e135df128ade6f73810a8f23f2c970c7fa5c971
brownzoem <brownzoem@gmail.com>
2023-11-27 16:31:17 -0500

    home page

--- meetbd/src/pages/HomePage.js
+++ meetbd/src/pages/HomePage.js
@@ -9,7 +9,42 @@ import Box from '@mui/material/Box';
 import { ClickAwayListener } from '@mui/base/ClickAwayListener';
 import { Portal } from '@mui/base/Portal';
 import Divider from '@mui/material/Divider';
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { PostgrestError } from '@supabase/supabase-js'
+
 const HomePage = () => {
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [userId, setUserId] = React.useState(null);
+    const [myEvents, setMyEvents] = React.useState([]);
+    const supabase = useSupabaseClient();
+
+    async function loadMyEvents() {
+        const { data: { user }, } = await supabase.auth.getUser();
+        setIsLoading(true)
+        try {
+            let { data, error } = await supabase
+                .from("events")
+                .select(`
+                    name,
+                    groups( name)
+
+                `)
+                .eq("event_owner", [user?.id]);
+
+            if (error) throw error;
+            if (data) setMyEvents(data);
+            console.log(data)
+        } catch (error) {
+            alert(error.error_description || error.message);
+        }
+
+        setIsLoading(false)
+
+    }
+    React.useEffect(() => {
+        loadMyEvents();
+    }, []);
+
     return (
         <Container>
             <Grid container spacing={5} sx={{ mt: 1 }}>
@@ -29,8 +64,30 @@ const HomePage = () => {
                     <Typography variant="h6" gutterBottom>
                         Created Events
                     </Typography>
-                    <p>Events I created that is still pending for me to schedule</p>
-                    <p>Group: Senior Group Project Event: Advisor meeting 2/5 responded</p>
+                    <p>Pending Events I Created: </p>
+                    <div>
+
+                        {myEvents.map((event) => (
+                            /*      <p>{JSON.stringify(event)}</p>*/
+                            <div>
+                                <Divider />
+                                <Typography variant="h9" sx={{ mr: 1 }} align="left" >
+                                    Group:
+                                </Typography>
+                                <Typography variant="h9" color='#BA7B92' sx={{ mr: 2 }} align="left">
+                                    {event.groups.name} 
+                                </Typography>
+                                <Typography variant="h9" sx={{ mr: 1 }} align="right">
+                                    Event:
+                                </Typography>
+                                <Typography variant="h9" color='#689f38' align="right">
+                                    {event.name}
+                                </Typography>
+                            </div>
+                            ))
+                        }
+                    </div>
+
                 </Grid>
             </Grid>
         </Container>
