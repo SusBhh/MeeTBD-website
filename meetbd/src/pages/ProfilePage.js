efd3eedf60ef9b5eaca255205790bc6b6345ec69
Isabel Mitre <isamitre@gmail.com>
2023-11-29 22:18:27 -0500

    allow changing of name

--- meetbd/src/pages/ProfilePage.js
+++ meetbd/src/pages/ProfilePage.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
-import { PostgrestError } from "@supabase/supabase-js";
 import { useState } from "react";
-import ReactDOM from "react-dom/client";
 
 const ProfilePage = () => {
-  const [profile, setProfile] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [userID, setUserID] = React.useState(null);
   const [displayName, setDisplayName] = useState("");
   const supabase = useSupabaseClient();
 
@@ -15,21 +16,25 @@ const ProfilePage = () => {
     const {
       data: { user },
     } = await supabase.auth.getUser();
+    setUserID(user.id);
 
     const { data: memberData, error: memberError } = await supabase
       .from("profile")
       .select("*")
       .eq("id", user.id);
-    setProfile(memberData[0]);
+    if (memberError) throw memberError;
     setDisplayName(memberData[0].display_name);
   };
-  getProfile();
+
+  React.useEffect(() => {
+    getProfile();
+  }, [supabase]);
 
   async function insertDisplayName() {
     const { data, error } = await supabase
       .from("profile")
       .update({ display_name: displayName })
-      .eq("id", profile.id)
+      .eq("id", userID)
       .single();
     if (error) {
       console.error("Error changing display name:", error.message);
@@ -53,17 +58,23 @@ const ProfilePage = () => {
     <Container>
       <Grid container spacing={5} sx={{ mt: 1 }}>
         <Grid item xs={12} md={8}>
-          <form onSubmit={handleSubmit}>
-            <label>
-              Change Display Name:{" "}
-              <input
-                type="text"
-                value={displayName}
-                onChange={(e) => setDisplayName(e.target.value)}
-              />{" "}
-            </label>
-            <input type="submit" />
-          </form>
+          {isLoading ? (
+            <CircularProgress />
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <label>
+                Change Display Name:{" "}
+                <input
+                  type="text"
+                  value={displayName}
+                  onChange={(e) => setDisplayName(e.target.value)}
+                />{" "}
+              </label>
+              <Button type="submit" variant="contained" size="small">
+                Submit
+              </Button>
+            </form>
+          )}
         </Grid>
       </Grid>
     </Container>
