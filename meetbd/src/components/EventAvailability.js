fa4c249a520420cb1ed3bfee5ee21d4e5a7a5246
Thorsten Lannynd <38738139+thorstenl312@users.noreply.github.com>
2023-12-04 15:17:28 -0500

    Update EventAvailability.js

--- meetbd/src/components/EventAvailability.js
+++ meetbd/src/components/EventAvailability.js
@@ -114,13 +114,13 @@ const EventAvailability = (props) => {
 
                     const updatedCells = [...curr.cells];
 
-                    for (let currentDate = startDateTime; currentDate <= endDateTime; currentDate.setHours(currentDate.getHours() + 1)) {
+                    for (let currentDate = startDateTime; currentDate <= endDateTime;) {
                         const daysDifference = Math.floor((currentDate - startDate) / (1000 * 60 * 60 * 24));
 
                         const timeDifference = currentDate.getHours() - parseInt(hours[0]);
                         
-                        updatedCells[timeDifference + 1][daysDifference + 1] = true; 
-                        if(currentDate > endDateTime) break;  
+                        updatedCells[timeDifference + 1][daysDifference + 1] = true;  
+                        currentDate.setHours(currentDate.getHours() + 1)
                     }
                     changeCurr({ cells: updatedCells });
 
