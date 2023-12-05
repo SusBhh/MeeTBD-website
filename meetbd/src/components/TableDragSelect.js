ea7f7a95286e7452d51cf0f6f5e4adf74101c74e
Isabel Mitre <isamitre@gmail.com>
2023-12-04 23:00:17 -0500

    reduce cognitive complexity of TableDragSelect

--- meetbd/src/components/TableDragSelect.js
+++ meetbd/src/components/TableDragSelect.js
@@ -134,28 +134,29 @@ export default class TableDragSelect extends React.Component {
     };
 
     handleTouchMoveCell = (e) => {
-        if (this.state.selectionStarted) {
-            e.preventDefault();
-            const { row, column } = eventToCellLocation(e);
-            const { startRow, startColumn, endRow, endColumn } = this.state;
+        if (!this.state.selectionStarted) {
+            return;
+        }
+        e.preventDefault();
+        const { row, column } = eventToCellLocation(e);
+        const { startRow, startColumn, endRow, endColumn } = this.state;
 
-            if (endRow !== row || endColumn !== column) {
-                const nextRowCount =
-                    startRow === null && endRow === null
-                        ? 0
-                        : Math.abs(row - startRow) + 1;
-                const nextColumnCount =
-                    startColumn === null && endColumn === null
-                        ? 0
-                        : Math.abs(column - startColumn) + 1;
+        if (endRow !== row || endColumn !== column) {
+            const nextRowCount =
+                startRow === null && endRow === null
+                    ? 0
+                    : Math.abs(row - startRow) + 1;
+            const nextColumnCount =
+                startColumn === null && endColumn === null
+                    ? 0
+                    : Math.abs(column - startColumn) + 1;
 
-                if (nextRowCount <= this.props.maxRows) {
-                    this.setState({ endRow: row });
-                }
+            if (nextRowCount <= this.props.maxRows) {
+                this.setState({ endRow: row });
+            }
 
-                if (nextColumnCount <= this.props.maxColumns) {
-                    this.setState({ endColumn: column });
-                }
+            if (nextColumnCount <= this.props.maxColumns) {
+                this.setState({ endColumn: column });
             }
         }
     };
